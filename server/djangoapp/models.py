17549b064a5c594abd918911a1dbc1889451e03c
Joseph McEvoy <joseph_mce@live.co.uk>
2026-01-16 12:38:38 -0500

    Added CarModel and CarMake Models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,67 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country_of_origin = models.CharField(max_length=100, blank=True)
+    logo = models.ImageField(upload_to='car_makes/logos/', blank=True, null=True)
+    is_premium = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name  # Return the name as the string representation
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('SALOON', 'Saloon'),
+        ('HATCHBACK', 'Hatchback'),
+        ('ESTATE', 'Estate'),
+        ('SUV', 'SUV'),
+        ('COUPE', 'Coupe'),
+        ('CONVERTIBLE', 'Convertible'),
+        ('MPV', 'MPV'),
+        ('CROSSOVER', 'Crossover'),
+        ('PICKUP', 'Pickup'),
+        ('VAN', 'Van'),
+        ('MINIBUS', 'Minibus'),
+        ('ROADSTER', 'Roadster'),
+        ('FASTBACK', 'Fastback')
+    ]
+    type = models.CharField(max_length=11, choices=CAR_TYPES, default='SALOON')
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ])
+    engine_size = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True)
+    fuel_type = models.CharField(
+        max_length=20, 
+        choices=(('PETROL', 'Petrol'), ('DIESEL', 'Diesel'), ('HYBRID', 'Hybrid'), ('ELECTRIC', 'Electric')),
+        blank=True
+    )
+    gearbox = models.CharField(
+        max_length=20, 
+        choices=(('MANUAL', 'Manual'), ('AUTOMATIC', 'Automatic')),
+        blank=True
+    )
+    doors = models.PositiveSmallIntegerField(blank=True, null=True)
+    seats = models.PositiveSmallIntegerField(blank=True, null=True)
+    colour = models.CharField(max_length=30, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    mileage = models.PositiveIntegerField(blank=True, null=True)  #in miles/km
+    is_available = models.BooleanField(default=True)
+    main_image = models.ImageField(upload_to='car_models/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name  # Return the name as the string representation
