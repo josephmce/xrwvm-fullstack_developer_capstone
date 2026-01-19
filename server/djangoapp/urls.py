4c97675b21c19b4011be62ed8df45c7db60b0b12
Joseph McEvoy <joseph_mce@live.co.uk>
2026-01-19 07:00:52 -0500

    Fixed login and registration

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,10 +7,13 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     path(route='get_cars', view=views.get_cars, name ='getcars'),
-    # # path for registration
-
+    # path for registration
+    path(route='register', view=views.registration, name='register'), 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    path(route='login', view=views.login_user, name='login'),
+
+    # path for logout
+    path(route='logout', view=views.logout, name='logout'),
 
     # path for dealer reviews view
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
@@ -22,7 +25,7 @@ urlpatterns = [
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
     # path for adding a review 
-    (route='add_review', view=views.add_review, name='add_review'),
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 
 
