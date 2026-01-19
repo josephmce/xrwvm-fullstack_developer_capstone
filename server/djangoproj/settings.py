9005d89e3610a8493f14606e3bf3f1bd68778605
Joseph MCEvoy <joseph_mce@live.co.uk>
2026-01-19 15:32:25 -0500

    Linting fix

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,11 +31,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://josephmce-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://josephmce-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.'
+    'cognitiveclass.ai',
 ]
+
 CSRF_TRUSTED_ORIGINS = [
-    'https://josephmce-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://josephmce-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.'
+    'cognitiveclass.ai',
 ]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
